440076d90421c5e0a25f2eac5f517ae19764e06f
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2022-03-29 23:32:44 +0200

    Add signin screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'screens/signin_screen/signin_screen.dart';
 
 Map<String, WidgetBuilder> routes = {
-  
+  SigninScreen.routeName: (context) => SigninScreen(),
 };
---
+++ lib/screens/signin_screen/signin_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constants.dart';
+
+class SigninScreen extends StatelessWidget {
+  static const routeName = '/signin-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign in Screen'),
+      ),
+      body: Center(
+        child: Text("Signin Screen"),
+      ),
+    );
+  }
+}
